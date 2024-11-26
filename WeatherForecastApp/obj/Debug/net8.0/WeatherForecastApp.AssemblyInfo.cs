0a12f7f25832197853d03b952605a2214423c857
RiFe202 <ericvampyr209@gmail.com>
2024-11-26 18:17:57 +0100

    1

--- WeatherForecastApp/obj/Debug/net8.0/WeatherForecastApp.AssemblyInfo.cs
+++ WeatherForecastApp/obj/Debug/net8.0/WeatherForecastApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cb4925fd336c6499d1aa28742887e1d85d1a944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ef495386aa04268b4460ee5e45f3d4e812c866")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecastApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
